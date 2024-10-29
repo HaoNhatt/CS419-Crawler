4523a1d6c896d5dcf49c20ad999ebd9924c251d4
HaoNhat <haonhatht@gmail.com>
2024-10-30 00:57:33 +0700

    Small update

--- crawler.py
+++ crawler.py
@@ -12,7 +12,14 @@ driver = webdriver.Chrome(service=service)
 
 # driver.get("https://google.com")
 # driver.get("https://tuoitre.vn")
-driver.get("https://tuoitre.vn/thoi-su/trang-5.htm")
+driver.get("https://tuoitre.vn/thoi-su.htm")
+
+categories = driver.find_elements(By.CLASS_NAME, "nav-link")
+# print(categories)
+for category in categories:
+    category_name = category.text
+    category_link = category.get_attribute("href")
+    print(f"Category: {category_name} - Link: {category_link}")
 
 WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.CLASS_NAME, "box-category-item"))
@@ -21,6 +28,6 @@ WebDriverWait(driver, 5).until(
 elements = driver.find_elements(By.CLASS_NAME, "box-category-item")
 print(elements)
 
-time.sleep(10)
+time.sleep(5)
 
 driver.quit()
